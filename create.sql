a3c8432fafa854d09571436aa068b2e5bb3da171
Jliang010 <107303015+Jliang010@users.noreply.github.com>
2024-04-18 18:08:54 -0700

    Update create.sql
    Theater and screen

--- create.sql
+++ create.sql
@@ -1,3 +1,16 @@
 -- Create tables here
 hi
 :D
+CREATE table Theater (
+TheaterID INT NOT NULL PRIMARY KEY,
+TheaterName varchar(60),
+City varchar(60)
+);
+
+CREATE TABLE Screen (
+    ScreenID INT PRIMARY KEY,
+    TheaterID INT,
+    Format VARCHAR(8),
+    FOREIGN KEY (TheaterID) REFERENCES Theater(TheaterID)
+);
+
