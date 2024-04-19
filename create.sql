786b2234ce61b0ca7f5e5d755a288ea2fa8ad82b
lucentmike <95521322+lucentmike@users.noreply.github.com>
2024-04-19 10:08:53 -0700

    Update create.sql

--- create.sql
+++ create.sql
@@ -5,13 +5,13 @@
 
 CREATE table Theater (
 TheaterID INT NOT NULL PRIMARY KEY,
-TheaterName varchar(60),
+TheaterName varchar(60) NOT NULL,
 City varchar(60)
 );
 
 CREATE TABLE Screen (
     ScreenID INT PRIMARY KEY,
-    TheaterID INT,
+    TheaterID INT NOT NULL,
     Format VARCHAR(8),
     FOREIGN KEY (TheaterID) REFERENCES Theater(TheaterID),
     CONSTRAINT chk_Format CHECK (Format in ('Standard', 'IMAX', '4DX'))
@@ -20,7 +20,7 @@ CREATE TABLE Screen (
 CREATE TABLE Movie (
   MovieID 	INT NOT NULL PRIMARY KEY,
   Title		VARCHAR(60) NOT NULL,
-  Rating 	VARCHAR(4),
+  Rating 	VARCHAR(4) NOT NULL,
   CONSTRAINT chk_Rating CHECK (Rating in ('PG', 'PG13', 'R'))
 );
 
@@ -28,17 +28,17 @@ CREATE TABLE Showing (
     ShowingID INT NOT NULL, 
     MovieID INT NOT NULL, 
     ScreenID INT NOT NULL, 
-    Date DATETIME,
+    Date DATETIME NOT NULL,
 CONSTRAINT PK_Presentation PRIMARY KEY (ShowingID),
 CONSTRAINT FK_MovieID FOREIGN KEY(MovieID) REFERENCES Movie (MovieID),
 CONSTRAINT FK_ScreenID FOREIGN KEY(ScreenID) REFERENCES Screen (ScreenID) 
 );
 
 CREATE TABLE Ticket(
-TicketID INT NOT NULL AUTO_INCREMENT, 
+TicketID INT NOT NULL, 
 ShowingID INT NOT NULL,
-Price DECIMAL(5,2),
-Seat VARCHAR(3),
+Price DECIMAL(5,2) NOT NULL,
+Seat VARCHAR(3) NOT NULL,
 CONSTRAINT PK_Ticket PRIMARY KEY (TicketID),
 CONSTRAINT FK_TicketShowingID FOREIGN KEY (ShowingID) REFERENCES Showing (ShowingID)
 );
